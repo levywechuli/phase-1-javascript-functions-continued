22b64720c30f44e1b516fbedf8eccd9ecef1461c
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-09-25 05:58:05 +0300

    overridding

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // code your solution here
+function saturdayFun(activity=`roller-skate`){
+    return ('This Saturday, I want to roller-skate!')
+}
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+console.log(saturdayFun()); 
+console.log(saturdayFun("bathe my dog"));
+
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+
+console.log(mondayWork()); 
+console.log(mondayWork("work from home"));
+
+
+function wrapAdjective(wrapper) {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
+
+
+const emphasize = wrapAdjective('*');
+console.log(emphasize("awesome")); 
+
+const highlight = wrapAdjective('||');
+console.log(highlight("fantastic"));
+
+
+
+
+
